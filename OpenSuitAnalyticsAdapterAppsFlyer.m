fe715a056182e8afbca5afbbc273e920d8cc928b
raojiafeng <raojiafeng@yodo1.com>
2022-04-06 18:29:21 +0800

    v1.1.7

--- OpenSuitAnalyticsAdapterAppsFlyer.m
+++ OpenSuitAnalyticsAdapterAppsFlyer.m
@@ -186,9 +186,7 @@ NSString* const OPENSUIT_ANALYTICS_APPSFLYER_APPLE_APPID   = @"AppleAppId";
 // deeplink
 - (BOOL)sendApplicationOfContinueUserActivity:(NSNotification *)noti {
     NSDictionary *dict = noti.userInfo;
-    if ([dict[@"userActivity"] length] == 0) {
-        [AppsFlyerLib.shared continueUserActivity:nil restorationHandler:nil];
-    } else {
+    if (dict[@"userActivity"]) {
         [AppsFlyerLib.shared continueUserActivity:dict[@"userActivity"] restorationHandler:nil];
     }
     
